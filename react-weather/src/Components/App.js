644e7615a28aebe1c93ee8d6ec707db7e16bbe9f
nicole stanton <nicole.stanton@ao.com>
2020-07-30 11:09:32 +0100

    NS - added day and date into forecast

--- react-weather/src/Components/App.js
+++ react-weather/src/Components/App.js
@@ -1,53 +1,48 @@
-import React, {
-  useEffect,
-  useReducer
-} from "react";
-import Weather from './Weather'
-import Forecast from './Forecast'
+import React, { useEffect, useReducer } from "react";
+import Weather from "./Weather";
+import Forecast from "./Forecast";
 
-import '../../src/App.css'
+import "../../src/App.css";
 
 const weatherUrl =
   "http://api.weatherapi.com/v1/forecast.json?key=ce12d95b27ae402395370111202907&q=London&days=3";
-
 
 const initialState = {
   loading: true,
   location: [],
   currentWeather: [],
   errorMessage: null,
-  forecast: []
-}
+  forecast: [],
+};
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'forecast_loading':
+    case "forecast_loading":
       return {
         ...state,
         loading: true,
-          errorMessage: null,
-      }
-      case 'forecast_success':
-        return {
-          ...state,
-          loading: false,
-            location: action.payload,
-            currentWeather: action.currentWeather,
-            forecast: action.forecast.forecastday
-        }
-        case 'forecast_failed':
-          return {
-            ...state,
-            loading: false,
-              errorMessage: action.error,
-          }
-          default:
-            return state;
+        errorMessage: null,
+      };
+    case "forecast_success":
+      return {
+        ...state,
+        loading: false,
+        location: action.payload,
+        currentWeather: action.currentWeather,
+        forecast: action.forecast.forecastday,
+      };
+    case "forecast_failed":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error,
+      };
+    default:
+      return state;
   }
-}
+};
 
 function App() {
-
   const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
@@ -55,49 +50,34 @@ function App() {
       .then((response) => response.json())
       .then((jsonResponse) => {
         dispatch({
-          type: 'forecast_success',
+          type: "forecast_success",
           payload: jsonResponse.location,
           currentWeather: jsonResponse.current,
-          forecast: jsonResponse.forecast
-        })
-        // console.log('current',jsonResponse.current);
-        // console.log('location',jsonResponse.location);
+          forecast: jsonResponse.forecast,
+        });
 
-        console.log('generic', jsonResponse)
-      })
-  }, [])
+        console.log("generic", jsonResponse);
+      });
+  }, []);
 
-  const {
-    location,
-    errorMessage,
-    loading,
-    currentWeather,
-    forecast
-  } = state;
-  // console.log('location',location)
-  // console.log('current',currentWeather)
-  console.log('forecast', forecast)
-
-
+  const { location, errorMessage, loading, currentWeather, forecast } = state;
 
   return (
     <div className="App">
-      <Weather location={location} currentWeather={currentWeather}/>
+      <Weather location={location} currentWeather={currentWeather} />
       <div className="forecast">
         {loading && !errorMessage ? (
           <span>loading...</span>
         ) : errorMessage ? (
           <div className="errorMessage">{errorMessage}</div>
         ) : (
-          forecast.map((forecast, index)=>(
-            <Forecast key={`${index}`} forecast={forecast}/>
-        ))
+          forecast.map((forecast, index) => (
+            <Forecast key={`${index}`} forecast={forecast} />
+          ))
         )}
       </div>
     </div>
-  )
+  );
+}
 
-};
-
-
-  export default App
+export default App;
--- react-weather/src/Components/Forecast.js
+++ react-weather/src/Components/Forecast.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React from "react";
 
-export function Forecast({ forecast }){
+export function Forecast({ forecast }) {
+  const epochTimestamp = forecast.date_epoch;
+  const displayDate = new Date(); // The 0 there is the key, which sets the date to the epoch
+  displayDate.setUTCSeconds(epochTimestamp);
+  const dayName = displayDate.toString().split(" ")[0];
+  const date = displayDate.toString().split(" ")[1];
+  const month = displayDate.toString().split(" ")[2];
 
-    return(
-        <React.Fragment>
-            <div className='weather-forecast'>
-                <span className='weather-forecast_day'>{forecast.date}</span>
-                <span className='weather-forecast_temp'>{forecast.day.avgtemp_c}°C</span>
-            </div>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <div className="weather-forecast">
+        <span className="weather-forecast_day">{`${dayName} ${date} ${month}`}</span>
+        <span className="weather-forecast_temp">
+          {forecast.day.avgtemp_c}°C
+        </span>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Forecast;
